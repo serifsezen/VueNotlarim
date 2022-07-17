b550ad9effb4716bb50842621acf67b8eeb96e54
Şerif Sezen <serifsezen23@gmail.com>
2022-07-17 20:08:27 +0300

    ders ekleme işlemi gerçekleştirildi

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,11 @@
 <template>
-    <div>
+    <div class="helloclass">
         <h1>Burasi HelloWorld</h1>
+         <form class="form" @submit.prevent="dersEkle">
+            <input type="text" placeholder="Ders Ekle" v-model="dersler"/>
+        </form>
         <ul>
-            <li v-for="(veri,index) in veriler" :key="index">{{index}} {{veri.ders}}</li>
+            <li v-for="(veri,index) in veriler" :key="index">{{index+1}} {{veri.ders}}</li>
         </ul>
         <p v-if="veriler.length>0"> Ders sayisi: {{veriler.length}}</p>
         <p v-else>Dersler eklenmemis</p>
@@ -14,15 +17,52 @@
 export default {
     data(){
         return{
+            dersler:"",
             veriler:[
-                {"ders":"Matematik"},
-                {"ders":"Fizik"},
+                // {"ders":"Matematik"},
+                // {"ders":"Fizik"},
             ]
+        }
+    },
+    methods:{
+        dersEkle(){
+            this.veriler.push({ders:this.dersler})
+            this.dersler='';
         }
     }
 }
 </script>
 
 <style>
-
+.helloclass{
+    background-color: rgb(236, 228, 228);
+}
+ul{
+    list-style-type: none;
+}
+ul li{
+    padding: 5px;
+    background-color:rgb(228, 222, 222);
+    border-left: 5px solid;
+    border-right: 5px solid;
+    margin-bottom: 2px;
+    margin-right: 30px;
+}
+p{
+    text-align: center;
+    padding: 5px;
+}
+h1{
+    padding: 5px;
+    text-align: center;
+}
+.form{
+    text-align: center;
+}
+input{
+    
+    border: 0;
+    padding: 5px;
+    box-shadow: 0px 0px 12px #888;
+}
 </style>
